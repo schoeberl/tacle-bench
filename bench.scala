1c99632790cf3b0ee4ffb40e324fde17e2601082
Martin Schoeberl <martin@jopdesign.com>
2015-07-09 23:33:02 +0200

    Explore different compiler flags

--- bench.scala
+++ bench.scala
@@ -16,15 +16,51 @@ import scala.language.postfixOps
 
 // println("Benchmarking Patmos")
 
+// Probably too many options...
+
+val CFLAGS_MIN = "-O2"
+
+// Partially a big difference between CFLAGS_MIN and CFLAGS
+// method cache numbers?
+// sqrt is 2 times faster with CFLAGS
+// statemate is 2 times faster with CFLAGS_MIN
+
+val CFLAGS = "-g -target patmos-unknown-unknown-elf -O2 " +
+        "-mpatmos-disable-stack-cache " +
+        "-mpatmos-disable-vliw " +
+        "-mpatmos-method-cache-size=0x1000 -mpatmos-preferred-subfunction-size=0 " +
+        "-mpatmos-stack-base=0x200000 -mpatmos-shadow-stack-base=0x1ff000"
+
+val CFLAGS_SC = "-g -target patmos-unknown-unknown-elf -O2 " +
+        "-mpatmos-disable-vliw " +
+        "-mpatmos-method-cache-size=0x1000 -mpatmos-preferred-subfunction-size=0 " +
+        "-mpatmos-stack-base=0x200000 -mpatmos-shadow-stack-base=0x1f8000 "
+
+
+
+val selection = Seq(
+"benchmarks/sequential/DSPstone_fixed_point/adpcm_g721_board_test1",
+"benchmarks/sequential/DSPstone_fixed_point/adpcm_g721_verify1",
+// "benchmarks/sequential/DSPstone_floating_point/matrix1_float1",
+"benchmarks/sequential/MISC/codecs_dcodhuff1",
+"benchmarks/sequential/MRTC/bsort1001",
+"benchmarks/sequential/MRTC/fft11",
+"benchmarks/sequential/MRTC/matmult1",
+"benchmarks/sequential/MRTC/prime1",
+"benchmarks/sequential/MRTC/sqrt1",
+"benchmarks/sequential/MRTC/statemate1",
+"benchmarks/sequential/MediaBench/cjpeg_jpeg6b_wrbmp1"
+)
+
 
 val dirs = "ls -R benchmarks/sequential" #| "grep :"!!
 
 def fileExists(name: String) = Seq("test", "-f", name).! == 0
 
-def doit(s: String) = {
+def doit(s: String, cflags: String) = {
   val d = s.substring(0, s.length-1)
   // println(d)
-  val cmd = Seq("sh", "-c", "patmos-clang -O2 "+d+"/*.c -o "+d+"/a.out")
+  val cmd = Seq("sh", "-c", "patmos-clang "+cflags+" "+d+"/*.c -o "+d+"/a.out")
   cmd!
   // pasim prints results to stderr...
   val run = Seq("sh", "-c", "pasim -v "+d+"/a.out 2>tmp.txt >/dev/null")
@@ -39,9 +75,9 @@ def doit(s: String) = {
       else 0
     // following does not work when the simulator exits != 0
     // val tim = (run #| "grep Cycles:")!!
-    if (cycles > 100000 && cycles < 1000000) {
+    // if (cycles > 100000 && cycles < 1000000) {
       println(result+" "+d)
-    }
+    // }
   } else {
     ""
   }
@@ -54,5 +90,11 @@ val lines = dirs.split("\\n")
 val some = Seq(lines(0), lines(1), lines(2), lines(3), lines(4))
 val abc = Seq("benchmarks/sequential/MISC/codecs_dcodrle1:", "benchmarks/sequential/MISC/g721_encode:")
 val mrtc = Seq("benchmarks/sequential/MRTC/adpcm_encoder:", "benchmarks/sequential/MRTC/binarysearch:")
-lines.map{ d => doit(d) }
+
+println("CFLAGS_MIN:")
+selection.map{ d => doit(d, CFLAGS_MIN) }
+println("CFLAGS:")
+selection.map{ d => doit(d, CFLAGS) }
+println("CFLAGS_SC:")
+selection.map{ d => doit(d, CFLAGS_SC) }
 
